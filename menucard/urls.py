ba804f766c4f8897de1476d9e1049dd239563976
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-11-18 14:15:08 +0530

    menucard

--- menucard/urls.py
+++ menucard/urls.py
@@ -5,7 +5,7 @@ app_name = 'menucard'
 
 urlpatterns = [
     path('menucard/<int:id>',views.home, name='home'),
-    path('product',views.products, name='product'),
+    path('product/<int:id>',views.products, name='product'),
     path('cart',views.cart, name='cart'),
     path('ordersuccess',views.orderSuccess, name='ordersuccess'),
 ]
